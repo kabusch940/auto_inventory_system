986473a739b67e96ba7fbb864c86b26595e29e53
kabusch-sec <81918182+kabusch940@users.noreply.github.com>
2025-12-02 18:59:45 +0100

    refactor inventory management logic and update JSON structure

--- AutomaticInventorySystem.ps1
+++ AutomaticInventorySystem.ps1
@@ -1,28 +1,30 @@
 $inventoryJSON = @{
     "Hosts" = $hosts
     "IPs" = $ips
+    "osVersions" = $osVersions
 }
 
 ## Initialize Arrays
 
 $ips = @()
 $hostnames = @()
-$osVersions
-$
+$osVersions = @()
+
 
 
 ## Check JSON File
 
 $wd = Get-Location
 
-$inventoryExists = Test-Path -Path ".\auto_inventory_system\inventory.json"
+$inventoryExists = Test-Path -Path ".\inventory.json"
 
 
 if($inventoryExists -eq $false){
-    New-Item -Path ".\auto_inventory_system" -Name "inventory.json" -ItemType File
-    $newInventory = Read-Host "You don't have an inventory yet. Please Add The IP Adresses of the hosts you want to check. (Comma seperated)"
+    New-Item -Path ".\" -Name "inventory.json" -ItemType File
+    $inventoryObject | ConvertTo-Json -Depth 5 | Set-Content ".\inventory.json"
+    $newInventory = Read-Host "Inventory was created in current Path. Please Add The IP Adresses of the hosts you want to check. (Comma seperated)"
     $newInventory = $newInventory.Trim()
-    $newInventory = $newInventory-split "," | Where-Object { $_ -ne "" }
+    $newInventory = $newInventory-split "," | | ForEach-Object { $_.Trim()} | Where-Object { $_ -ne "" } 
     if ($newInventory -eq "") {
         Write-Host "Please enter some input."
     } else {
@@ -38,11 +40,14 @@ if($inventoryExists -eq $false){
 } 
 
 
+
 if($inventoryExists){
-    Write-Host "Inventory found."
+    $jsonData = Get-Content -Path ".\inventory.json" -Raw
+    $inventoryObject = $jsonData | ConvertFrom-Json -Depth 5
+    Write-Host "Inventory loaded."
     $newInventory = Read-Host "Please Add The IP Adresses of the hosts you want to check. (Comma seperated)"
     $newInventory = $newInventory.Trim()
-    $newInventory = $newInventory-split "," | Where-Object { $_ -ne "" }
+    $newInventory = $newInventory-split "," | ForEach-Object { $_.Trim()} | Where-Object { $_ -ne "" }
     $newInventory
     if ($newInventory -eq "") {
         Write-Host "Please enter some input."
@@ -58,6 +63,23 @@ if($inventoryExists){
     }
 } 
 
-$inventoryJSON.IPs
 
-$inventoryJSON | ConvertTo-Json -Depth 5 | Set-Content -Path ".\auto_inventory_system\inventory.json"
+## Save IP to JSON
+
+foreach($newIP in $newInventory){
+    $inventoryObject.IPs += $newIP
+}
+
+$inventoryObject | ConvertTo-Json -Depth 5 | Set-Content .\inventory.json
+Write-Host "Inventory Updated."
+
+## Inventory Object
+
+$inventoryObject = @{
+    IPs         = $ips
+    osVersions           = $osVersions
+    hostnames             = $hostnames
+    LastUpdated         = (Get-Date)
+}
+
+
