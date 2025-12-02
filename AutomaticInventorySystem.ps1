497b91f174e78d37c760a69c7041f8a950a3aef2
kabusch-sec <81918182+kabusch940@users.noreply.github.com>
2025-12-02 20:01:00 +0100

    refactor inventory management script and enhance JSON structure

--- AutomaticInventorySystem.ps1
+++ AutomaticInventorySystem.ps1
@@ -1,15 +1,11 @@
-$inventoryJSON = @{
-    "Hosts" = $hosts
-    "IPs" = $ips
-    "osVersions" = $osVersions
-}
-
 ## Initialize Arrays
 
 $ips = @()
 $hostnames = @()
 $osVersions = @()
-
+$online = [System.Collections.ArrayList]@()
+$offline = [System.Collections.ArrayList]@()
+$LastPing = [System.Collections.ArrayList]@()
 
 
 ## Check JSON File
@@ -19,20 +15,22 @@ $wd = Get-Location
 $inventoryExists = Test-Path -Path ".\inventory.json"
 
 
+## Create new File and Set Content
+
 if($inventoryExists -eq $false){
-    New-Item -Path ".\" -Name "inventory.json" -ItemType File
-    $inventoryObject | ConvertTo-Json -Depth 5 | Set-Content ".\inventory.json"
+    $createdInventory = New-Item -Path ".\" -Name "inventory.json" -ItemType File
+    $inventoryObject | ConvertTo-Json | Set-Content $createdInventory
     $newInventory = Read-Host "Inventory was created in current Path. Please Add The IP Adresses of the hosts you want to check. (Comma seperated)"
     $newInventory = $newInventory.Trim()
-    $newInventory = $newInventory-split "," | | ForEach-Object { $_.Trim()} | Where-Object { $_ -ne "" } 
+    $newInventory = $newInventory-split "," | ForEach-Object { $_.Trim()} | Where-Object { $_ -ne "" } 
     if ($newInventory -eq "") {
         Write-Host "Please enter some input."
     } else {
-        foreach($input in $newInventory){
-            if([ipaddress]::TryParse($input, [ref]$null)){
-                $input += $ips
+        foreach($newHost in $newInventory){
+            if([ipaddress]::TryParse($newHost, [ref]$null)){
+                $ips += $newHost
             } else{
-                Write-Host $input "is not a valid IP-Adress."
+                Write-Host $newHost "is not a valid IP-Adress."
             }
         }
         
@@ -40,23 +38,24 @@ if($inventoryExists -eq $false){
 } 
 
 
+## Convert JSON to Object if File exists
 
 if($inventoryExists){
     $jsonData = Get-Content -Path ".\inventory.json" -Raw
     $inventoryObject = $jsonData | ConvertFrom-Json -Depth 5
+    $ips += $inventoryObject.IPs
     Write-Host "Inventory loaded."
     $newInventory = Read-Host "Please Add The IP Adresses of the hosts you want to check. (Comma seperated)"
     $newInventory = $newInventory.Trim()
     $newInventory = $newInventory-split "," | ForEach-Object { $_.Trim()} | Where-Object { $_ -ne "" }
-    $newInventory
     if ($newInventory -eq "") {
         Write-Host "Please enter some input."
     } else {
-        foreach($input in $newInventory){
-            if([ipaddress]::TryParse($input, [ref]$null)){
-                $input += $ips
+        foreach($newHost in $newInventory){
+            if([ipaddress]::TryParse($newHost, [ref]$null)){
+                $ips += $newHost
             } else{
-                Write-Host $input "is not a valid IP-Adress."
+                Write-Host $newHost "is not a valid IP-Adress."
             }
         }
         
@@ -64,22 +63,37 @@ if($inventoryExists){
 } 
 
 
-## Save IP to JSON
 
-foreach($newIP in $newInventory){
-    $inventoryObject.IPs += $newIP
+## Input Validation (duplicates)
+
+foreach($addedIP in $newInventory){
+    if($inventoryObject.IPs -contains $addedIP){
+        $inputValidation = Read-Host "IP already exists. Should the status be checked? (J/N)"
+    }
+}
+
+if($inputValidation -match "^[Jj]$"){
+    foreach($addedIP in $newInventory){
+        $ips = $ips | Where-Object { $_ -ne $addedIP }
+        ## function placeholder for WinRM check
+    }
+}
+
+
+
+
+## Inventory Object
+
+$inventoryObject        = @{
+    IPs                 = $ips
+    osVersions          = $osVersions
+    hostnames           = $hostnames
+    LastUpdated         = (Get-Date)
+    Online =            = $online
+    Offline =           = $offline
+    lastPing =          = $lastPing
 }
 
 $inventoryObject | ConvertTo-Json -Depth 5 | Set-Content .\inventory.json
 Write-Host "Inventory Updated."
-
-## Inventory Object
-
-$inventoryObject = @{
-    IPs         = $ips
-    osVersions           = $osVersions
-    hostnames             = $hostnames
-    LastUpdated         = (Get-Date)
-}
-
 
